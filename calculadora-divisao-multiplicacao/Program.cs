b6a84e074f41fc18d346d6c99504f887f20582d2
VitoriaArgolo7423 <vitoriaargolo8457@gmail.com>
2025-02-28 16:17:41 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora-divisao-multiplicacao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Calculadora
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("\nQual operação você deseja realizar?");
+        Console.WriteLine("Digite 5 para Multiplicação");
+        Console.WriteLine("Digite 6 para Divisão");
+
+        int escolha = Convert.ToInt32(Console.ReadLine());
+
+        switch (escolha)
+        {
+            case 5:
+                Console.WriteLine("Multiplicação: " + (num1 * num2));
+                break;
+
+            case 6:
+                Console.WriteLine("Divisão: " + (num1 / num2));
+                break;
+
+            default:
+                Console.WriteLine("Escolha inválida! Por favor, insira um número entre 5 e 6.");
+                break;
+        }
+    }
+
+
+}
